f402da0d23b68d5c2cfc8b9d3eac66ebac5729a3
marcelo.sagarcia@gmail.com <marcelo.sagarcia@gmail.com>
2014-07-09 16:23:13 -0300

    Initial Commit

---
+++ ZueiraFala/ALIViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALIViewController.h
+//  ZueiraFala
+//
+//  Created by Marcelo Garcia on 09/07/14.
+//  Copyright (c) 2014 com.bit4All.comqueroupa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALIViewController : UIViewController
+
+@end
